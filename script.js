3d3effc1453ec7dd6db497bc56f3bb75c3e23056
Victor Kawan <95768649+VictorKawan@users.noreply.github.com>
2023-10-14 14:14:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Aviso
 if(window.screen.width > 500){
-  console.log|('Esta página é responsiva apenas para dispositivos moveis') 
+  console.log('Esta página é responsiva apenas para dispositivos moveis') 
 }
 
 // Altura da Aba
